4fdab970b0be971c7e4bf71b69041d03f91f6277
Cendy Saputra <ce.createe@gmail.com>
2026-06-08 15:39:15 +0700

    kontak.blade.php (Settings informasi by dashboard)

--- resources/views/kontak.blade.php
+++ resources/views/kontak.blade.php
@@ -8,42 +8,50 @@
         ->filter()
         ->implode(' ');
 
-    $contact = [
-        [
-            'title_phone' => 'Nomor Telp',
-            'phone1' => '+ 62 000 0000 0000',
-            'phone2' => '+ 62 000 0000 0000',
-        ],
+    $globals = \Statamic\Facades\GlobalSet::findByHandle('settings')?->inCurrentSite()?->data();
 
-        [
-            'title_email' => 'Email',
-            'email1' => 'email@dummy.com',
-        ],
+    // Telepon (semua yang enabled)
+    $phones = collect($globals['phone_numbers'] ?? [])
+        ->filter(fn($item) => $item['enabled'] ?? false)
+        ->pluck('number')
+        ->filter()
+        ->values()
+        ->all();
 
-        [
-            'title_address' => 'Alamat',
-            'address' =>
-                'Jl. Lkr. Luar Barat No.9, RT.14/RW.3, Rw. Buaya, Kecamatan Cengkareng, Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11740',
-            'address_url' =>
-                'https://www.google.com/maps/search/?api=1&query=Jl.+Lkr.+Luar+Barat+No.9%2C+RT.14%2FRW.3%2C+Rw.+Buaya%2C+Kecamatan+Cengkareng%2C+Kota+Jakarta+Barat%2C+Daerah+Khusus+Ibukota+Jakarta+11740',
-        ],
-    ];
-    $socials = [
-        [
-            'name' => 'Instagram',
-            'link' => 'https://instagram.com/gayamakmurmobil',
-            'icon' => asset('assets/instagram.svg'),
-        ],
-        ['name' => 'Facebook', 'link' => 'https://facebook.com/fawindonesia/', 'icon' => asset('assets/facebook.svg')],
-        [
-            'name' => 'LinkedIn',
-            'link' => 'https://linkedin.com/company/fawindonesia',
-            'icon' => asset('assets/linkedin.svg'),
-        ],
-    ];
+    // Email (semua yang enabled)
+    $emails = collect($globals['emails'] ?? [])
+        ->filter(fn($item) => $item['enabled'] ?? false)
+        ->pluck('email')
+        ->filter()
+        ->values()
+        ->all();
 
-    $mapEmbed =
-        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.706041215713!2d106.72720077588374!3d-6.170101793817197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f785e478eed7%3A0x22011b641a6b1e4!2sGaya%20Makmur%20Mobil%20Pt.%2C%20Rw.%20Buaya%2C%20Kecamatan%20Cengkareng%2C%20Kota%20Jakarta%20Barat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2011740!5e0!3m2!1sen!2sid!4v1779440004920!5m2!1sen!2sid" width="100%" height="450" style="border:0;width:100%;height:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+    // Alamat & link maps
+    $address = $globals['address'] ?? null;
+    $addressUrl = $globals['google_map_link'] ?? null;
+
+    // Embed maps (iframe lengkap dari YAML)
+    $mapEmbed = $globals['embed_google_maps'] ?? null;
+
+    // Sosmed
+    $socials = collect($globals['social_media'] ?? [])
+        ->map(function ($item, $key) {
+            $url = $item['url'] ?? null;
+            if (!$url) {
+                return null;
+            }
+
+            $iconRaw = $item['image'] ?? null;
+
+            return [
+                'name' => ucfirst($key),
+                'link' => $url,
+                'icon' => $iconRaw ? asset('assets/' . $iconRaw) : null,
+            ];
+        })
+        ->filter()
+        ->values()
+        ->all();
 
     $formText = collect($page->sections)->first(fn($section) => (string) $section['identifier'] === 'text-form');
 
@@ -88,87 +96,80 @@
                             {{-- Konten kontak --}}
                             <div class="flex flex-col gap-8">
 
-                                @foreach ($contact as $item)
-                                    <div
-                                        class="flex flex-col gap-2 {{ $loop->first ? '' : 'border-t border-(--color-line) mt-6 pt-6' }}">
-                                        {{-- Nomor telepon --}}
-                                        @if (isset($item['title_phone']))
-                                            <div id="phone-number" class="flex flex-col gap-2">
-                                                <span
-                                                    class="title-display text-xl text-(--color-primary)">{{ $item['title_phone'] }}</span>
-                                                <div class="flex flex-col gap-1">
+                                {{-- Nomor telepon --}}
+                                @if (count($phones) > 0)
+                                    <div id="phone-number" class="flex flex-col gap-2">
+                                        <span class="title-display text-xl text-(--color-primary)">Nomor Telp</span>
+                                        <div class="flex flex-col gap-1">
+                                            @foreach ($phones as $phone)
+                                                <a href="tel:{{ preg_replace('/\s+/', '', $phone) }}"
+                                                    class="text-(--color-body) hover:text-(--color-secondary)">
+                                                    {{ $phone }}
+                                                </a>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
 
-                                                    <a href="tel:{{ preg_replace('/\s+/', '', $item['phone1']) }}"
-                                                        class="text-(--color-body) hover:text-(--color-secondary)">
-                                                        {{ $item['phone1'] }}
-                                                    </a>
+                                {{-- Email --}}
+                                @if (count($emails) > 0)
+                                    <div id="email"
+                                        class="flex flex-col gap-2 border-t border-(--color-line) mt-6 pt-6">
+                                        <span class="title-display text-(--color-primary)">Email</span>
+                                        <div class="flex flex-col gap-1">
+                                            @foreach ($emails as $email)
+                                                <a href="mailto:{{ $email }}"
+                                                    class="text-(--color-body) hover:text-(--color-secondary)">
+                                                    {{ $email }}
+                                                </a>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                @endif
 
-                                                    <a href="tel:{{ preg_replace('/\s+/', '', $item['phone2']) }}"
-                                                        class="text-(--color-body) hover:text-(--color-secondary)">
-                                                        {{ $item['phone2'] }}
-                                                    </a>
-
-                                                </div>
-                                            </div>
+                                {{-- Alamat kantor --}}
+                                @if ($address)
+                                    <div id="address-office"
+                                        class="flex flex-col gap-2 border-t border-(--color-line) mt-6 pt-6">
+                                        <span class="title-display text-(--color-primary)">Alamat</span>
+                                        @if (!empty($addressUrl))
+                                            <a href="{{ $addressUrl }}" target="_blank" rel="noopener noreferrer"
+                                                class="text-(--color-body) hover:text-(--color-secondary) lg:w-[90%]">
+                                                {{ $address }}
+                                            </a>
+                                        @else
+                                            <p>{{ $address }}</p>
                                         @endif
-
-                                        {{-- Email --}}
-                                        @if (isset($item['title_email']))
-                                            <div id="email" class="flex flex-col gap-2">
-                                                <span
-                                                    class="title-display text-(--color-primary)">{{ $item['title_email'] }}</span>
-                                                <div class="flex flex-col gap-1">
-
-                                                    <a href="mailto:{{ $item['email1'] }}"
-                                                        class="text-(--color-body) hover:text-(--color-secondary)">
-                                                        {{ $item['email1'] }}
-                                                    </a>
-
-                                                </div>
-                                            </div>
-                                        @endif
-
-                                        {{-- Alamat kantor --}}
-                                        @if (isset($item['title_address']))
-                                            <div id="address-office" class="flex flex-col gap-2">
-
-                                                <span
-                                                    class="title-display text-(--color-primary)">{{ $item['title_address'] }}</span>
-                                                @if (!empty($item['address_url']))
-                                                    <a href="{{ $item['address_url'] }}" target="_blank"
-                                                        rel="noopener noreferrer"
-                                                        class="text-(--color-body) hover:text-(--color-secondary) lg:w-[90%]">
-                                                        {{ $item['address'] }}
-                                                    </a>
-                                                @else
-                                                    <p>{{ $item['address'] }}</p>
-                                                @endif
-
-                                            </div>
-                                        @endif
-                                @endforeach
+                                    </div>
+                                @endif
 
                                 {{-- Media sosial --}}
-                                <div id="social-media"
-                                    class="flex flex-col gap-4 border-t border-(--color-line) mt-6 pt-6">
-                                    <span class="title-display text-(--color-primary)">Media Sosial</span>
-                                    <div class="flex gap-6">
-                                        @foreach ($socials as $social)
-                                            <a href="{{ $social['link'] }}" target="_blank" rel="noopener noreferrer"
-                                                title="{{ $social['name'] }}" class="hover:text-(--color-primary)">
-                                                <span class="social-icon block w-5 h-5 social-icon-grey"
-                                                    style="--icon-url: url('{{ $social['icon'] }}');"></span>
-                                            </a>
-                                        @endforeach
+                                @if (count($socials) > 0)
+                                    <div id="social-media"
+                                        class="flex flex-col gap-4 border-t border-(--color-line) mt-6 pt-6">
+                                        <span class="title-display text-(--color-primary)">Media Sosial</span>
+                                        <div class="flex gap-6">
+                                            @foreach ($socials as $social)
+                                                <a href="{{ $social['link'] }}" target="_blank"
+                                                    rel="noopener noreferrer" title="{{ $social['name'] }}"
+                                                    class="hover:text-(--color-primary)">
+                                                    <span class="social-icon block w-5 h-5 social-icon-grey"
+                                                        style="--icon-url: url('{{ $social['icon'] }}');"></span>
+                                                </a>
+                                            @endforeach
+                                        </div>
                                     </div>
-                                </div>
+                                @endif
 
                             </div>
 
                             {{-- Maps --}}
-                            <div class="mt-10 h-75 overflow-hidden rounded-2xl lg:rounded-3xl">
-                                {!! $mapEmbed !!}
-                            </div>
+                            @if ($mapEmbed)
+                                <div
+                                    class="mt-10 h-75 overflow-hidden rounded-2xl lg:rounded-3xl [&_iframe]:w-full [&_iframe]:h-full">
+                                    {!! $mapEmbed !!}
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
