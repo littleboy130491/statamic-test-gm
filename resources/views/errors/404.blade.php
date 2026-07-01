c177dee507bb04c83b2212369d6c87419ffcccda
Cendy Saputra <ce.createe@gmail.com>
2026-07-01 20:34:45 +0700

    Dev 404 Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,56 @@
-<x-layouts.app>
-    <p>404 Page not found.</p>
-</x-layouts.app>
+@php
+    $bodyClass = collect([
+        'background-grey',
+        $is_entry ?? false ? 'entry' : null,
+        isset($collection) ? 'entry-' . $collection : null,
+        isset($collection) ? $collection : null,
+        isset($slug) ? 'slug-' . $slug : null,
+    ])
+        ->filter()
+        ->implode(' ');
+
+    // Cek component
+    $hasHeader = view()->exists('components.layouts.header.single-header');
+    $hasFooter = view()->exists('components.layouts.footer.footer');
+
+    // Global label 404
+    $error = \Statamic\Facades\GlobalSet::findByHandle('page_label_not_found')
+        ?->in(\Statamic\Facades\Site::current()->handle())
+        ?->toAugmentedArray();
+
+    $notFound = $error['not_found'] ?? '404';
+    $heading = $error['heading'] ?? '';
+    $desc = $error['description'] ?? '';
+    $btnLabel = $error['button_label'] ?? '';
+    $btnLink = $error['link'] ?? '/';
+@endphp
+
+<x-layouts.main>
+    @if ($hasHeader)
+        <x-layouts.header.single-header />
+    @endif
+
+    <section id="error-404">
+        <div class="container">
+            <div class="my-18 md:my-18 lg:my-30 flex flex-col items-center relative">
+                <div class="relative w-fit">
+                    <p
+                        class="font-(family-name:--font-display) text-center text-[14rem] md:text-[16rem] lg:text-[25rem] leading-none font-semibold text-(--color-surface)/80">
+                        {{ $notFound }}</p>
+                    <h2 class="font-(family-name:--font-display) absolute inset-0 m-auto w-fit h-fit text-center">
+                        {{ $heading }}</h2>
+                </div>
+                <div class="w-full flex flex-col gap-3 justify-center items-center z-10 -mt-7 md:-mt-6 lg:-mt-14">
+                    <p class="text-center mb-4 w-full md:w-[55%] lg:w-[35%]">{{ $desc }}</p>
+                    @if ($btnLabel)
+                        <a href="{{ $btnLink }}" class="button button--secondary">{{ $btnLabel }}</a>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </section>
+
+    @if ($hasFooter)
+        <x-layouts.footer.footer />
+    @endif
+</x-layouts.main>
