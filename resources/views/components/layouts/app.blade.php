684556a30fa3d97d4f80d27a5e4120dbeadd8c24
Cendy Saputra <ce.createe@gmail.com>
2026-07-04 16:24:08 +0700

    Settings active Gtranslate

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,13 +1,16 @@
 <!doctype html>
-<html lang="{{ $site->short_locale ?? 'en' }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @seo_pro('meta')
-        @vite(['resources/css/site.css', 'resources/js/site.js'])
-    </head>
-    <body class="bg-white font-sans leading-normal text-zinc-800 antialiased">
-        {{ $slot }}
-    </body>
+<html lang="{{ $site->short_locale ?? 'id' }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @seo_pro('meta')
+    @vite(['resources/css/site.css', 'resources/js/site.js'])
+</head>
+
+<body class="bg-white font-sans leading-normal text-zinc-800 antialiased">
+    {{ $slot }}
+</body>
+
 </html>
